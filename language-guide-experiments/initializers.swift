50696be1275a945baf1cb292b3c5632c9cff422e
James Shapiro <jamesshapiro@gmail.com>
2017-06-06 23:59:42 -0700

    update inheritance

--- language-guide-experiments/initializers.swift
+++ language-guide-experiments/initializers.swift
@@ -28,3 +28,32 @@ let notALotOfBaht = InternationalAccount(fromBaht: 100)
 
 print(aLotOfBaht.usDollarsBalance)
 print(notALotOfBaht.usDollarsBalance)
+
+class UndraftedFreeAgent {
+    let fullName: String
+    // this property is optional because the UDFA
+    // may never get signed and may earn nothing.
+    // In fact, this is usually what happens.
+    var salary: Int?
+    init(fullName: String) {
+        self.fullName = fullName
+    }
+    func displayName() {
+        print(fullName)
+    }
+}
+
+let udfa = UndraftedFreeAgent(fullName: "Robert 'Fat Rob' Kelley")
+udfa.displayName()
+// We signed him!
+udfa.salary = 540_000
+
+class NaturalNumbers {
+    var one = 1
+    var two = 2
+    var three = 3
+    var etCetera: String?
+}
+var naturalNumbers = NaturalNumbers()
+
+
