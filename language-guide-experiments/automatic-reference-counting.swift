e10791d046e515630efb5a92d3706a0e0cbb72e3
James Shapiro <jamesshapiro@gmail.com>
2017-06-15 23:12:14 -0700

    update automatic-reference-counting

--- language-guide-experiments/automatic-reference-counting.swift
+++ language-guide-experiments/automatic-reference-counting.swift
@@ -56,3 +56,55 @@ crowderJersey!.player = crowder
 crowder = nil
 crowderJersey = nil
 // Strong reference cycle, so neither instance is deinitialized (!)
+
+class Contract {
+    let salary: Int
+    init(salary: Int) { self.salary = salary }
+    var player: Player3?
+    deinit { print("contract worth \(salary) is being deinitialized") }
+}
+
+class Player3 {
+    let name: String
+    init(name: String) { self.name = name }
+    weak var contract: Contract?
+    deinit { print("Player \(name) is being deinitialized") }
+}
+
+var mattJones: Player3?
+var contract: Contract?
+
+mattJones = Player3(name: "Matt Jones")
+contract = Contract(salary: 629_000)
+
+mattJones!.contract = contract
+contract!.player = mattJones
+
+// Prints "contract worth 629000 is being deinitialized"
+contract = nil
+
+// Prints "Player Matt Jones is being deinitialized"
+mattJones = nil
+
+class Customer {
+    let name: String
+    var card: CreditCard?
+    init(name: String) { self.name = name }
+    deinit { print("\(name) is being deinitialized") }
+}
+
+class CreditCard {
+    let number: UInt64
+    unowned let customer: Customer
+    init(number: UInt64, customer: Customer) {
+        self.number = number
+        self.customer = customer
+    }
+    deinit { print("Card #\(number) is being deinitialized") }
+}
+
+var bashaud: Customer?
+bashaud = Customer(name: "Bashaud Breeland")
+bashaud!.card = CreditCard(number: 2626_2266_2222_6666, customer: bashaud!)
+
+bashaud = nil
